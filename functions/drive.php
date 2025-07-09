49adb7654b107c5e834395ffc0b280bf081d71cf
ml-Dev-K <kanemouhamadoulamine50@gmail.com>
2025-07-09 10:02:11 +0200

    Update drive.php

--- functions/drive.php
+++ functions/drive.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
 
 // Google Client (Service Account)
